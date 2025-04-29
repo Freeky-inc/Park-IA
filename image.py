ea513aca330e3b82f887b20a1a29133d610fcb05
bnjjs <baptist1998.jaques@gmail.com>
2025-04-29 12:03:58 +0100

    premiere image

--- image.py
+++ image.py
@@ -1,1 +1,12 @@
 import cv2
+import numpy as np
+
+images =cv2.imread('images/test.jpg')
+
+for i in range(images.shape[0]):
+    for j in range(images.shape[1]):
+        images[i,j] = [255,255,255]
+
+cv2.imshow('images', images)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
